e964ff121810d43732b2418fe640497b8ec135be
mdeiml <matthias@deiml.net>
2016-07-11 21:51:32 +0200

    Init libgdx

---
+++ android/src/com/mdeiml/minild69/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.mdeiml.minild69;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.mdeiml.minild69.MiniLD69;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new MiniLD69(), config);
+	}
+}
---
+++ desktop/src/com/mdeiml/minild69/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.mdeiml.minild69.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mdeiml.minild69.MiniLD69;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MiniLD69(), config);
+	}
+}
